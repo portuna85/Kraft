8790616a590d95984f51042f99e1cdd26e006d67
portuna85 <portuna85@gmail.com>
2026-05-24 23:21:51 +0900

    .

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,5 @@
 const { defineConfig, devices } = require('@playwright/test');
+const gradleCmd = process.platform === 'win32' ? 'gradlew.bat' : './gradlew';
 
 module.exports = defineConfig({
   testDir: './tests/e2e',
@@ -6,7 +7,7 @@ module.exports = defineConfig({
   retries: 1,
   reporter: 'list',
   webServer: {
-    command: 'gradlew.bat bootRun --args="--server.port=18080 --spring.profiles.active=local --kraft.skip.required-config-validator=true --spring.datasource.url=jdbc:h2:mem:e2e;MODE=MySQL;DB_CLOSE_DELAY=-1;DATABASE_TO_LOWER=TRUE --spring.datasource.driver-class-name=org.h2.Driver --spring.datasource.username=sa --spring.datasource.password= --spring.jpa.hibernate.ddl-auto=create-drop --spring.flyway.enabled=false --kraft.db.connectivity-check.enabled=false --kraft.security.ops.enabled=false"',
+    command: `${gradleCmd} bootRun --args="--server.port=18080 --spring.profiles.active=local --kraft.skip.required-config-validator=true --spring.datasource.url=jdbc:h2:mem:e2e;MODE=MySQL;DB_CLOSE_DELAY=-1;DATABASE_TO_LOWER=TRUE --spring.datasource.driver-class-name=org.h2.Driver --spring.datasource.username=sa --spring.datasource.password= --spring.jpa.hibernate.ddl-auto=create-drop --spring.flyway.enabled=false --kraft.db.connectivity-check.enabled=false --kraft.security.ops.enabled=false"`,
     url: 'http://localhost:18080',
     reuseExistingServer: false,
     timeout: 120000,
